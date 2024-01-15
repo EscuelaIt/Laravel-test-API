fdb5acd0b17cc9bdbb602c94fc64047793e9c6a4
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2024-01-15 20:11:02 +0100

    fortify

--- config/cors.php
+++ config/cors.php
@@ -14,7 +14,7 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-
+    // 'paths' => [],
     // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'logout'],
     // 'paths' => ['*'],
